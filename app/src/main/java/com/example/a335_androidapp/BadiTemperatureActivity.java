ca3266e6976cac8ffa6b211fb56df2558e173088
Blaser Tobias <bblast@bbc-be.local>
2019-05-06 14:31:56 +0200

    started with weather api

---
+++ app/src/main/java/com/example/a335_androidapp/BadiTemperatureActivity.java
@@ -0,0 +1,75 @@
+package com.example.a335_androidapp;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.a335_androidapp.helper.WieWarmJsonParser;
+import com.example.a335_androidapp.model.Badi;
+import com.example.a335_androidapp.model.Becken;
+
+import org.json.JSONException;
+
+public class BadiTemperatureActivity extends AppCompatActivity {
+    private static final String endpoint = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private static final String appId = "&APPID=59ee4acb1201b53089d449b38595bc16";
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_badi_temperature);
+    }
+
+    private void generateAlertDialog() {
+        progressBar.setVisibility(View.GONE);
+        AlertDialog.Builder dialogBuilder;
+        dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // Closes this activity
+                finish();
+            }
+        });
+        dialogBuilder.setMessage("Die Temeratur konnte nicht geladen werden. Versuche es später nochmals.").setTitle("Fehler");
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+    }
+
+    private void getBadiTemp(String url) {
+        final ArrayAdapter<Becken> beckenInfosAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1);
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    Badi badi = WieWarmJsonParser.createBadiFromJsonString(response);
+                    beckenInfosAdapter.addAll(badi.getBecken());
+                    ListView badiInfoList = findViewById(R.id.beckenliste);
+                    badiInfoList.setAdapter(beckenInfosAdapter);
+                    progressBar.setVisibility(View.GONE);
+                } catch (JSONException e) {
+                    generateAlertDialog();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                generateAlertDialog();
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+
+}
